b6b1dba6a6f3197319eefb474ffa434b59a83133
Hesham Ali <hesham.ali@integrant.com>
2021-06-27 15:16:58 +0200

    fixing room entities

--- app/src/main/java/com/example/valorant_agents/features/agentsList/Models/RoomAgent.kt
+++ app/src/main/java/com/example/valorant_agents/features/agentsList/Models/RoomAgent.kt
@@ -1,11 +1,10 @@
 package com.example.roomwordsample.Entities
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.ColumnInfo
-import androidx.room.TypeConverter
+import androidx.room.*
 import com.example.valorant_agents.features.agentsList.Models.Ability
 import com.example.valorant_agents.features.agentsList.Models.Role
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
@@ -16,7 +15,7 @@ import kotlinx.serialization.json.encodeToJsonElement
 
 @Entity(tableName = "agent_table")
 data class RoomAgent(
-        //@ColumnInfo(name = "abilities") val abilities: ArrayList<RoomAbility>,
+        @ColumnInfo(name = "abilities") val abilities: List<RoomAbility>,
         @ColumnInfo(name = "assetPath") val assetPath: String,
         @ColumnInfo(name = "bustPortrait") val bustPortrait: String,
         @ColumnInfo(name = "characterTags") val characterTags: List<String>?,
@@ -30,7 +29,7 @@ data class RoomAgent(
         @ColumnInfo(name = "isFullPortraitRightFacing") val isFullPortraitRightFacing: Boolean,
         @ColumnInfo(name = "isPlayableCharacter") val isPlayableCharacter: Boolean,
         @ColumnInfo(name = "killfeedPortrait") val killfeedPortrait: String,
-        //@ColumnInfo(name = "role") val role: Role,
+        @Embedded(prefix = "character_") val role: RoomRole,
         @PrimaryKey @ColumnInfo(name = "uuid") val uuid: Int)
 
 @Entity(tableName = "agent_ability")
@@ -53,8 +52,20 @@ data class RoomRole(
 
 class Converters {
         @TypeConverter
-        fun fromList(value : List<String>) = Json.encodeToString(value)
+        fun fromRoomAbilityList(value : List<RoomAbility>) : String {
+                val type = object : TypeToken<List<RoomAbility?>?>() {}.type
+                return Gson().toJson(value, type)
+        }
 
         @TypeConverter
-        fun toList(value: String) = Json.decodeFromString<List<String>>(value)
+        fun toRoomAbilityList(value: String) : List<RoomAbility> {
+                val type = object : TypeToken<List<RoomAbility?>?>() {}.type
+                return Gson().fromJson<List<RoomAbility>>(value, type)
+        }
+
+        @TypeConverter
+        fun stringListToJson(value: List<String>?) = Gson().toJson(value)
+
+        @TypeConverter
+        fun jsonToStringList(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
 }
